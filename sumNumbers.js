119a4e9633618cbf50346590df055106fd3a82c9
Denis Mihaylov <116539046+deniskmihaylov@users.noreply.github.com>
2022-12-09 23:42:29 +0000

    Add files via upload

---
+++ sumNumbers.js
@@ -0,0 +1,14 @@
+function sumNumber (input) {
+
+    let n = Number(input[0]); 
+    let sum = 0; 
+    let index = 1; 
+
+    while (sum < n) {
+        let currentNumber = Number(input[index]); 
+        sum += currentNumber; 
+        index++
+    } console.log(sum)
+}
+
+sumNumber ([100, 10, 20, 30, 40])
